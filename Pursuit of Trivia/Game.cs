954e2a02fee970efd0f60fea650240f6518d6b95
tkyprios <114837393+tkyprios@users.noreply.github.com>
2025-09-03 13:16:25 +1000

    project commit

---
+++ Pursuit of Trivia/Game.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pursuit_of_Trivia
+{
+    internal class Game
+    {
+        public Table GameTable { get; set; }
+
+        public Player currentPlayer { get; set; }
+
+
+        public Game() 
+        {
+            GameTable = new Table();
+        }
+        public void Setup()
+        {
+            Console.Clear();
+            Console.WriteLine("What would you like your username to be?");
+            string playerUsername = Console.ReadLine();
+
+            currentPlayer = new Player(playerUsername);
+            Console.WriteLine($"Welcome, {currentPlayer.Name}! Press any key to start the game...");
+            Console.ReadKey(true);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Pursuit of Trivia/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pursuit_of_Trivia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                ShowWelcomeScreen();
+                Game game = new Game();
+                game.Setup();
+            }
+        }
+
+
+        private static void ShowWelcomeScreen()
+        {
+            Console.Clear();
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("-- Welcome to Pursuit of Trivia! --");
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Press any key to start the game...");
+            Console.ReadKey(true);
+        }
+    }
+}
