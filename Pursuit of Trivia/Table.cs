0b38fdebb91c4fb4ee31af0111e435ab62400417
tkyprios <114837393+tkyprios@users.noreply.github.com>
2025-09-09 12:51:13 +1000

    add player functionality to Table class

--- Pursuit of Trivia/Table.cs
+++ Pursuit of Trivia/Table.cs
@@ -8,5 +8,28 @@ namespace Pursuit_of_Trivia
 {
     internal class Table
     {
+        public List<Player> Players { get; private set; }
+        public Table()
+        {
+            Players = new List<Player>();
+        // @TODO generate questions, add question cards (class) and answers, card deck per category?
+        }
+
+        public void AddPlayerToGame(Player player)
+        {
+            Players.Add(player);
+            Console.WriteLine($"Player {player.Name} has been added to the game!");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey(true);
+        }
+
+        public void AddBotToGame()
+        {
+            Player bot = new Player("Bot");
+            Players.Add(bot);
+            Console.WriteLine("A bot has been added to the game!");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey(true);
+        }
     }
 }
