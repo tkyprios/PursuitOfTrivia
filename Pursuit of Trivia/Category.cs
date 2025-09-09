dcd63964c42c29802c33f72bd7bbc474785fe917
tkyprios <114837393+tkyprios@users.noreply.github.com>
2025-09-09 14:18:46 +1000

    Create Category Enum

---
+++ Pursuit of Trivia/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pursuit_of_Trivia
+{
+    public enum Category
+    {
+        Characters = 1,
+        PlanetsAndLocations = 2,
+        ShipsVehiclesAndTech = 3,
+        EventsAndBattles = 4,
+        QuotesAndLore = 5
+
+    }
+}
